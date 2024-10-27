bea1b29eef940b48796757edbe640263e6a10069
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-10-27 22:24:59 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,4 +5,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://ctfuser:ctfpass@db:5432/ctfdb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SERVICE_AUTH_SECRET = os.environ.get('SERVICE_AUTH_SECRET', 'very_secret_key_456')
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max-body-size - important for smuggling
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max-body-size
