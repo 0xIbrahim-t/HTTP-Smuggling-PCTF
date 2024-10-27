d6a63e5183317dfdf004a2860f2ac344b7112bbc
t-ibrahim-t <9600074747asma2@gmail.com>
2024-10-28 00:09:24 +0530

    delete collector

--- http-smuggling-ctf/backend/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('JWT_SECRET', 'super_secret_key_123')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://ctfuser:ctfpass@db:5432/ctfdb')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVICE_AUTH_SECRET = os.environ.get('SERVICE_AUTH_SECRET', 'very_secret_key_456')
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max-body-size - important for smuggling
